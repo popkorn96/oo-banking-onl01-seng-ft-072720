a153c6786088eb969df9267c10cdaf6134f98643
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-11 23:16:11 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,11 +13,7 @@ class BankAccount
     "Your balance is $#{balance}."
   end
   def valid?
-    if status == "open" && balance > 0
-      true 
-    else
-      false
-    end
+    self.status == "open" && self.balance > 0 ? true : false
   end
   def close_account
     @status = "closed"
