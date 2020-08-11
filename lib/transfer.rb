e8c83e2b10a7743a3f51c82c494f64a047ea61ff
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-11 23:20:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
     @receiver.balance+=amount
     @status = "complete"
   else valid?
-    self.status == "rejected"
+    self.status = "rejected"
     return "Transaction rejected. Please check your account balance."
   end
  end
