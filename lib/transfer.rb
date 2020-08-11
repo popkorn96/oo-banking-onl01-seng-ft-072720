8219aa44fa939e0aa49a5345b1d25975ea473208
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-11 23:19:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,7 +11,7 @@ class Transfer
    
  end
  def execute_transaction
-   if valid? && @sender.balance > @amount && @status == "pending"
+   if valid? && @sender.balance > amount && self.status == "pending"
     @sender.balance-=@amount
     @receiver.balance+=@amount
     @status = "complete"
